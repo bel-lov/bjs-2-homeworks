5ca9f9482d23f98268eddb5237b663bed6c46bbb
bel-lov <belou.lyuba@yandex.ru>
2022-03-18 20:04:29 +0300

    added task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,13 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  const d = Math.pow (b, 2) - 4*a*c;
+  if (d === 0){
+    arr.push(-b/(2*a));
+  }
+  else if (d > 0){
+    arr.push((-b + Math.sqrt(d) )/(2*a)), arr.push((-b - Math.sqrt(d) )/(2*a));
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
