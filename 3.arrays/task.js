d35e9e29f96c63ccb8aa4a2fc64d8438cdb6313f
bel-lov <belou.lyuba@yandex.ru>
2022-03-25 14:07:05 +0300

    added task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,25 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  //let result;
 
-  // Ваш код
+  let result =  arr1.length === arr2.length && arr1.every((item,i)=> item === arr2[i]);
 
   return result; // boolean
 }
 
+/*function compareArrays(arr1, arr2) {
+
+  let result = (arr1.every(
+  function compareArrays1(item,i) {
+      return arr1.length === arr2.length && arr1[i] === arr2[i]
+        }
+  ))
+  return result;
+  }
+*/
+
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((item)=> item > 0).filter((item)=>  item % 3 === 0).map((item) => item *10);
 
-  // Ваш код
-
+  
   return resultArr; // array
 }
