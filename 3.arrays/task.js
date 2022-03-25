8b457b28c96eb19a415902ee7f7f357df03f9406
bel-lov <belou.lyuba@yandex.ru>
2022-03-25 22:02:16 +0300

    new task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,5 @@
 function compareArrays(arr1, arr2) {
-  //let result;
-
-  let result =  arr1.length === arr2.length && arr1.every((item,i)=> item === arr2[i]);
-
-  return result; // boolean
+  return result =  arr1.length === arr2.length && arr1.every((item,i)=> item === arr2[i]);
 }
 
 /*function compareArrays(arr1, arr2) {
@@ -18,8 +14,5 @@ function compareArrays(arr1, arr2) {
 */
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((item)=> item > 0).filter((item)=>  item % 3 === 0).map((item) => item *10);
-
-  
-  return resultArr; // array
+  return resultArr = arr.filter((item)=> item > 0).filter((item)=>  item % 3 === 0).map((item) => item *10); 
 }
