82723d5cbfb48e947c2a5a4cd7282af31ec655a2
bel-lov <belou.lyuba@yandex.ru>
2022-04-18 21:14:15 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name,
+    this.gender = gender,
+    this.age = age,
 
-}
-
+    
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark){
+  if(this.marks === undefined){  
+     this.marks = [mark];
+    } else {
+     this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...mark){
+   this.marks = mark;
+}
+
+Student.prototype.getAverage = function(){
+  let s = 0;
+  for (let i = 0; i < this.marks.length; i++){
+      s += this.marks[i]
+   }
+   return s/this.marks.length;
+}
+
+Student.prototype.exclude = function(reason){
+  delete this.marks 
+  delete this.subject
+  this.excluded  = reason;
+   } 
+
+}
+const student1 = new Student();
+const student2 = new Student();
+const student3 = new Student();
