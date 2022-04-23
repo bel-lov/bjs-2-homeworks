0aa9c2b5c957fccb555986902d753ae6c5694ab5
bel-lov <belou.lyuba@yandex.ru>
2022-04-23 10:14:52 +0300

    second commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,37 +4,41 @@ function Student(name, gender, age) {
     this.age = age,
 
     
-Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+      Student.prototype.setSubject = function (subjectName) {
+      this.subject = subjectName;
+      }
+
+      Student.prototype.addMark = function(mark){
+      if(this.marks === undefined){  
+         this.marks = [mark];
+         } else {
+         this.marks.push(mark);
+         }
+      }
+
+      Student.prototype.addMarks = function(...mark){
+         if(this.marks ===undefined){
+            this.marks = [];
+         }
+         return this.marks.push(...mark);
+      }
+
+      Student.prototype.getAverage = function(){
+      let s = 0;
+      for (let i = 0; i < this.marks.length; i++){
+            s += this.marks[i]
+         }
+         return s/this.marks.length;
+      }
+
+      Student.prototype.exclude = function(reason){
+      delete this.marks 
+      delete this.subject
+      this.excluded  = reason;
+         } 
+
 }
 
-Student.prototype.addMark = function(mark){
-  if(this.marks === undefined){  
-     this.marks = [mark];
-    } else {
-     this.marks.push(mark);
-    }
-}
-
-Student.prototype.addMarks = function(...mark){
-   this.marks = mark;
-}
-
-Student.prototype.getAverage = function(){
-  let s = 0;
-  for (let i = 0; i < this.marks.length; i++){
-      s += this.marks[i]
-   }
-   return s/this.marks.length;
-}
-
-Student.prototype.exclude = function(reason){
-  delete this.marks 
-  delete this.subject
-  this.excluded  = reason;
-   } 
-
-}
 const student1 = new Student();
 const student2 = new Student();
 const student3 = new Student();
