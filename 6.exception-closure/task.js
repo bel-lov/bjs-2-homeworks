3d39e81144f270e07faec449df86ee1b49943ad6
bel-lov <belou.lyuba@yandex.ru>
2022-04-28 16:50:26 +0300

    second commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,53 +1,53 @@
-function parseCount(value){
-    let x = Number.parseInt(value);
-    if (isNaN(x)){
-      throw new Error ("Невалидное значение");
+function parseCount(value) {
+  let x = Number.parseInt(value);
+  if (isNaN(x)) {
+    throw new Error("Невалидное значение");
+  }
+  return x;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  }
+  catch (err) {
+    return (err);
+  }
+}
+  
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if ((a + b) < c || (a + c) < b || (b + c) < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
     }
-    return x;
+  }
+  getPerimeter() {
+    return this.a + this.b + this.c
   }
 
-  function validateCount(value){
-    try{
-      validateCount(value)
-      return parseCount(value);
-    }
-    catch(err){
-     return(err);
-    } 
+  getArea() {
+    const s = this.getPerimeter()/ 2;
+    const area = Math.sqrt(s * ((s - this.a) * (s - this.b) * (s - this.c)));
+    return parseFloat(area.toFixed(3));
   }
-  class Triangle{
-      constructor(a, b, c){
-          this.a = a;
-          this.b = b;
-          this.c = c;
-          if((a+b)<c || (a+c) < b || (b + c) < a ){
-            throw new Error ("Треугольник с такими сторонами не существует");
-          }
-      }
-      getPerimeter(){
-              return this.a +  this.b +this.c
-      }
+}
 
-      getArea(){
-        const s = (this.a + this.b + this.c) / 2;
-        const area = Math.sqrt(s * ((s - this.a) * (s - this.b) * (s - this.c)));
-               return parseFloat(area.toFixed(3));
-      }
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
   }
-
-  function getTriangle(a, b, c){
-    try {
-      return new Triangle(a, b, c);
-    }
-    catch {
-      return {
-        getPerimeter(){
-          return ("Ошибка! Треугольник не существует")
-        },
-        getArea(){
-          return ("Ошибка! Треугольник не существует")
-        }
+  catch {
+    return {
+      getPerimeter() {
+        return ("Ошибка! Треугольник не существует")
+      },
+      getArea() {
+        return ("Ошибка! Треугольник не существует")
       }
     }
   }
+}
   
