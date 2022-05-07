6d3190ae48312f3a25294c073750b5d63f69cfbb
bel-lov <belou.lyuba@yandex.ru>
2022-05-07 22:18:37 +0300

    first commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,35 @@
+const addThree = (a, b, c) => a + b + c;
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');//args[0] + "," + args[1];
+    console.log(hash)
+    const idx = cache.findIndex((item)=> item.hash === hash);
+    if(idx !== -1){
+      console.log('Из кеша: ' + cache[idx].value)
+    } else {
+      let result = addThree(...args); 
+      if(cache.length < 5){
+       cache.push({ 'hash':hash, 'value':result}); 
+        console.log(cache)
+      }
+      else{
+        cache.shift();
+        cache.push({ 'hash':hash, 'value':result}); 
+      }
+      console.log("Вычисляем: " + result);
+    } 
+  }  
+  return wrapper;
 }
+
+
+const upgradedAddThree = cachingDecoratorNew(addThree);
+upgradedAddThree(1,2,3);
+
+
 
 
 function debounceDecoratorNew(func) {
